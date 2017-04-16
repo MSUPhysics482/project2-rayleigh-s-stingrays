998a83b0ac04172db68acebf3a15ed2fe51c0d30
Jane Kim <kimjane7@msu.edu>
2017-04-16 07:29:16 -0400

    Updated program and histogram (see spectrum.pdf)

--- project.cpp
+++ project.cpp
@@ -23,45 +23,27 @@ double random01(mt19937 generator){
 	return dist();
 }
 
-/*
-int main(int argc, char *argv[]) {
-
-	double R,I;
-	//double xmin=atof(argv[1]), xmax=atof(argv[2]), dx=10E-9;
-	double xmin=400E-9, xmax=700E-9, dx=10E-9; //visible wavelength range
-	double f=1E25; //scaling factor
-
-	ofstream file;
-	file.open("data.txt");
-
-	for(double x=xmin; x<xmax; x+=dx){
-
-		R=(2.0*h*c*c*pow(x,-5.0))*pow(exp(h*c/(x*kb*T))-1,-1.0);
-		I=pow(x,-4.0); //only wavelength dependence for now
-		file << x << "\t" << f*R << "\t" << I*R << "\n";
-	}
-
-	file.close();
-
-	return 0;
+double SR(double wavelength){
+	 return (2.0*h*c*c*pow(wavelength,-5.0))*pow(exp(h*c/(wavelength*kb*T))-1,-1.0);
 }
-*/
-
 
 
 int main(int argc, char *argv[]) {
 
     // spectral radiance, maximum spectral radiance, intensity;
-	double y,ymax=0,I; 
+	double y,yf,ymax=0,I; 
 	//visible wavelength range
-	double xmin=400E-9, xmax=700E-9, xdiff=xmax-xmin, xmid, dx=1E-9; 
+	double xmin=400E-9, xmax=700E-9, xdiff=xmax-xmin, xmid; 
 
 	// use wein's law to find maximum spectral radiance
-	ymax=(2.0*h*c*c*pow(b/T,-5.0))*pow(exp(h*c/(b*kb))-1,-1.0);
+	ymax=SR(b/T);
 
 	// set up for histogram
-	int ntot=10E5, nbins=30, ibin;
+	int ntot=10E4, nbins=50, ibin;
 	double before[nbins]={0.0}, after[nbins]={0.0}, binsize=xdiff/nbins;
+
+	// set up random number generator
+	mt19937 generator(time(0));
 
 	// open a data file
 	ofstream file;
@@ -74,61 +56,31 @@ int main(int argc, char *argv[]) {
 		// give names to random numbers 
 		double rx,ry;
 
-		// set up random number generator
-		mt19937 generator(time(0));
-
 		// throw a random wavelength in visible range
 		rx=xmin+xdiff*(random01(generator));
 
 		// calculate the theoretical spectral radiance at rx
-		y=(2.0*h*c*c*pow(rx,-5.0))*pow(exp(h*c/(rx*kb*T))-1,-1.0);
+		y=SR(rx);
 
-		// "keep=true" means we found a probable wavelength
-		bool keep=false;
-		while(keep==false){
+		ry=ymax*(random01(generator));
 
-			// throw a random spectral radiance ranging from 0 to ymax
-			ry=ymax*(random01(generator));
+		if(ry<y){
+			//file << rx << "\t" << ry << endl;
+			ibin=lrint(floor((rx-xmin)/binsize));
+			before[ibin]+=1.0;
 
-			// if ry<y, then keep the random wavelength rx
-			if(ry<y) {
-				keep=true;
-				//file << rx << "\t" << ry << endl;
-				ibin=lrint(floor((rx-xmin)/binsize));
-				before[ibin]+=1.0;
-			}
-
-			// if ry>y, we go back to the beginning of this loop
-			else continue;
 		}
+
 	}
-
-	/*
-
-	up to this point, the program is doing what we expect
-	we pick a random rx in the visible wavelength range 400-700 nm
-	then we pick a random ry in the range 0-ymax
-	so we have a random coordinate in the plane (rx,ry)
-	we compare (rx,ry) with (rx,B(rx)), where B is Planck's distribution function
-	we end up with a bunch of points (rx,ry) that only lie under the curve B
-
-	the value of ry doesn't really matter after this point
-	all that matters, is the number of rx's in a given range [x,x+dx]
-	if we made a histogram of all the rx coordinates we found, 
-	the histogram should be proportional to Planck's distribution
-	but it's not, so our next goal is to figure this out
-
-
-	*/
-
 
 
 	for(ibin=0; ibin<nbins; ibin++){
 		xmid=xmin+(ibin+0.5)*binsize;
 		y=(2.0*h*c*c*pow(xmid,-5.0))*pow(exp(h*c/(xmid*kb*T))-1,-1.0);
 		after[ibin]=before[ibin]*pow(xmid,-4.0);
-		file << xmid << "\t" << before[ibin] << "\t" << after[ibin] << "\n";
+		file << xmid*10E8 << "\t" << 1.5*pow(10,25)*before[ibin] << "\t" << after[ibin] << "\n";
 	}
+
 
 
 	file.close();	
@@ -144,8 +96,7 @@ int main(int argc, char *argv[]) {
 
 TO DO:
 
-- ERROR CHECK: random wavelengths are not proportional to Planck's distribution
-  need to go through each step and make sure it's working like we expect.
+- Make sure everything is scaled correctly.
 
 - Add dependency on radius of air molecules into intensity function. 
   Randomly scatter off N2 80% of the time and O2 20%. 
